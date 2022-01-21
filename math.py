2c0a9808333994565423aed01f19ac0752a7dbed
pratyushpare <98163579+pratyushpare@users.noreply.github.com>
2022-01-21 18:32:36 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+# Implement 
+def add(x,y) :
+    pass
+# Implement     
+def susbstract(x,y) :
+    return x-y          #on master branch
+# Implement     
+def multiply(x,y) :
+    return x*y          #on bug456
+# Implement 
+def divide(x,y) :
+    pass
